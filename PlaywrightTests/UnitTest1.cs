9986e21ecf8a87cff2d9c7619d34008c3c770008
tphu <tphu003200@gmail.com>
2022-05-23 16:42:58 +0700

    dasdad

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -15,7 +15,7 @@ public class MyTest : PageTest
     }
 
     [Test]
-    public async Task Test1()
+    public async Task Login()
     {
         // Click text=Login
         await Page.ClickAsync("text=Login");
@@ -25,5 +25,32 @@ public class MyTest : PageTest
         await Page.FillAsync("#Password", "password");
         await Page.ClickAsync("text=Log in");
         await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
+    }
+
+    public async Task SignUp()
+    {
+        // Click text=Register
+        await Page.Locator("text=Register").ClickAsync();
+        // Click input[name="UserName"]
+        await Page.Locator("input[name=\"UserName\"]").ClickAsync();
+        // Fill input[name="UserName"]
+        await Page.Locator("input[name=\"UserName\"]").FillAsync("tphu01253" + DateTime.Now.ToString("HH:mm:ss"));
+        // Click input[name="Password"]
+        await Page.Locator("input[name=\"Password\"]").ClickAsync();
+        // Double click input[name="Password"]
+        await Page.Locator("input[name=\"Password\"]").DblClickAsync();
+        // Fill input[name="Password"]
+        await Page.Locator("input[name=\"Password\"]").FillAsync("@t3PrR2ZyxSLQXQ");
+        // Click input[name="ConfirmPassword"]
+        await Page.Locator("input[name=\"ConfirmPassword\"]").ClickAsync();
+        // Fill input[name="ConfirmPassword"]
+        await Page.Locator("input[name=\"ConfirmPassword\"]").FillAsync("@t3PrR2ZyxSLQXQ");
+        // Click input[name="Email"]
+        await Page.Locator("input[name=\"Email\"]").ClickAsync();
+        // Fill input[name="Email"]
+        await Page.Locator("input[name=\"Email\"]").FillAsync("test123@gmail.com");
+        // Click input:has-text("Register")
+        await Page.Locator("input:has-text(\"Register\")").ClickAsync();
+        await Expect(Page.Locator("text=Log off")).ToBeVisibleAsync();
     }
 }
